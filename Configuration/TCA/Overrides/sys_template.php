366e561eab3acd804ab6192c19fe8e267ffb694f
Raphael Thanner <r.thanner@zeroseven.de>
2020-08-31 13:24:53 +0200

    Rename static template

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,5 +2,5 @@
 defined('TYPO3_MODE') || die();
 
 call_user_func(static function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('z7_blog_rss', 'Configuration/TypoScript','RSS Feed for Blog-Extension ');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('z7_blog_rss', 'Configuration/TypoScript','Blog (RSS-Feed)');
 });
