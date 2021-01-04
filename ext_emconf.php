13f398ac16f515bea9f3d1e792c3ea6092c2ca60
Raphael Thanner <r.thanner@zeroseven.de>
2021-01-04 10:19:32 +0000

    Compatibility update to z7_blog version 1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,11 +9,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'zeroseven design studios GmbH',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
-            'z7_blog' => '1.0.0-1.0.99'
+            'z7_blog' => '1.0.0-1.1.99'
         ]
     ]
 ];
