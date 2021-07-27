002cc46296721d8f41977eb47561308d2071df47
Raphael Thanner <r.thanner@zeroseven.de>
2021-07-27 09:52:10 +0000

    Update composer.json and ext_emconf.php to be installable in TYPO3 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.0-11.99.99',
             'z7_blog' => '1.0.0-1.1.99'
         ]
     ]
