4bdae9e0a315684610aedaa091c9c6dd5b9343b3
André Buchmann <a.buchmann@zeroseven.de>
2022-08-25 14:18:09 +0200

    Create php 8 compatibility

--- Classes/ViewHelpers/LanguageViewHelper.php
+++ Classes/ViewHelpers/LanguageViewHelper.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Zeroseven\Z7BlogRss\ViewHelpers;
 
-use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
 
 class LanguageViewHelper extends AbstractTagBasedViewHelper
@@ -13,7 +12,7 @@ class LanguageViewHelper extends AbstractTagBasedViewHelper
 
     public function render(): string
     {
-        if ($GLOBALS['TSFE'] instanceof TypoScriptFrontendController && $language = $GLOBALS['TSFE']->config['config']['language'] ?? null) {
+        if ($language = $GLOBALS['TSFE']->config['config']['language'] ?? null) {
             $this->tag->setContent($language);
             return $this->tag->render();
         }
